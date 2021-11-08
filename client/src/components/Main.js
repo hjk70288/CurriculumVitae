5ad8b731ce7c74937a95d5ad8b29a246a368d521
김용민 <hjk70288@naver.com>
2021-11-08 23:21:38 +0900

    tech skills 부분 수정

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -12,8 +12,10 @@ import react_icon from '../assets/image/skill/react.png';
 import jquery_icon from '../assets/image/skill/jquery.png';
 import aws_icon from '../assets/image/skill/aws.png';
 import nodejs_icon from '../assets/image/skill/nodejs.png';
+import jsp_icon from '../assets/image/skill/jsp.png';
 import maria_db_icon from '../assets/image/skill/maria_db.png';
 import git_icon from '../assets/image/skill/git.png';
+import linux_icon from '../assets/image/skill/linux.svg';
 import top_button_icon from '../assets/image/top_button.png';
 import Fade from 'react-reveal/Fade';
 import ReactPageScroller from "react-page-scroller";
@@ -207,33 +209,66 @@ function Main(){
 							<div className='title'>Tech Skills</div>
 							<div className='skill_container'>
 								<div className='skill_item'>
-									<div>Front-End</div>
+									<div className='skill_category'>Front-End</div>
 									<div className='skills'>
-										<img src={html_icon} alt=''></img>
-										<img src={css_icon} alt=''></img>
-										<img src={sass_icon} alt=''></img>
-										<img src={js_icon} alt=''></img>
-										<img src={jquery_icon} alt=''></img>
-										<img src={react_icon} alt=''></img>
+										<div className='skill'>
+											<img src={html_icon} alt=''></img>
+											<span>HTML5</span>
+										</div>
+										<div className='skill'>
+											<img src={css_icon} alt=''></img>
+											<span>CSS3</span>
+										</div>
+										<div className='skill'>
+											<img src={sass_icon} alt=''></img>
+											<span>SASS</span>
+										</div>
+										<div className='skill'>
+											<img src={js_icon} alt=''></img>
+											<span>JS ES6</span>
+										</div>
+										<div className='skill'>
+											<img src={jquery_icon} alt=''></img>
+											<span>JQuery</span>
+										</div>
+										<div className='skill'>
+											<img src={react_icon} alt=''></img>
+											<span>React JS</span>
+										</div>
 									</div>
 								</div>
 								<div className='skill_item'>
-									<div>Back-End</div>
+									<div className='skill_category'>Back-End</div>
 									<div className='skills'>
-										<img src={aws_icon} alt=''></img>
-										<img src={nodejs_icon} alt=''></img>
+										<div className='skill'>
+											<img src={nodejs_icon} alt=''></img>
+											<span>Node JS</span>
+										</div>
+										<div className='skill'>
+											<img src={jsp_icon} alt=''></img>
+											<span>Jsp</span>
+										</div>
+										<div className='skill'>
+											<img src={maria_db_icon} alt=''></img>
+											<span>Marai DB</span>
+										</div>
 									</div>
 								</div>
 								<div className='skill_item'>
-									<div>Database</div>
+									<div className='skill_category'>DevOps</div>
 									<div className='skills'>
-										<img src={maria_db_icon} alt=''></img>
-									</div>
-								</div>
-								<div className='skill_item'>
-									<div>Configuration Management</div>
-									<div className='skills'>
-										<img src={git_icon} alt=''></img>
+										<div className='skill'>
+											<img src={aws_icon} alt=''></img>
+											<span>AWS</span>
+										</div>
+										<div className='skill'>
+											<img src={git_icon} alt=''></img>
+											<span>Git</span>
+										</div>
+										<div className='skill'>
+											<img src={linux_icon} alt=''></img>
+											<span>Linux</span>
+										</div>
 									</div>
 								</div>
 							</div>
