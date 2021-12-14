894d5b0ccfb87a585c9316f2668add4f5543d30f
김용민 <hjk70288@naver.com>
2021-12-14 21:46:01 +0900

    프로젝트에 prettier 추가

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Main from './components/Main';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Main from "./components/Main";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Main}/>
-        </Switch>
-    </BrowserRouter>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Main} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
